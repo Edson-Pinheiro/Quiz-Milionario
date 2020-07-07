23a22464d5ec77e12659d512dd0b2a7ca4f0380b
Edson-Pinheiro <edrpj7@gmail.com>
2020-07-06 22:49:26 -0300

    Add projects Quiz

---
+++ js/apk.js
@@ -0,0 +1,234 @@
+
+//aqui faz a ligação dos textos com o index
+const questionNumber = document.querySelector(".question-number");
+const questionText = document.querySelector (".question-text");
+const optionContainer = document.querySelector (".option-container");
+const answersIndicatorConteiner = document.querySelector(".answers-indicator");
+const closeBox = document.querySelector(".quiz-box");
+const finalBox = document.querySelector(".final-box");
+const homeBox = document.querySelector(".home-box");
+const btn = document.querySelector(".next-question-btn");
+
+
+
+
+//const resultBox = document.querySelector(".result-box");
+
+//declaração das variavies locais
+let questionCounter = 0; // para contar as questões
+let currentQuestion; // aponta para a questão atual
+let availableQuestions = []; // array de perguntas
+let availableOptions = []; // array para listar as respostas
+
+
+function setAvailableQuestions(){
+	const totalquestion = quiz.length; // tirar??
+	for(let i=0; i < totalquestion; i++){
+		availableQuestions.push(quiz[i]);
+		//console.log(quiz[i],quizV);
+		//console.log([i],quizV[15]);
+		//availableQuestions = [i];
+		//console.log(availableQuestions = [i]);
+	}
+}
+
+function getNewQuestion(){
+	questionNumber.innerHTML = " Pergunta de número " + (questionCounter +1);
+	
+	const questionIndex = availableQuestions[questionCounter];
+	currentQuestion = questionIndex;
+	questionText.innerHTML = currentQuestion.q; // aqui mostra pergunta na tela
+	console.log(questionIndex)
+	
+	const optionLen = currentQuestion.options.length;
+	
+for (let i=0 ; i<optionLen ; i ++){
+		availableOptions.push(i)		
+	}
+	console.log(availableOptions);	
+	
+	optionContainer.innerHTML = '';   // limpa tela
+	
+		
+	//cria opções de respostas no html
+	for (let i=0 ; i<optionLen ; i ++){
+		// sorteia as option
+		const optonIndex = availableOptions[Math.floor(Math.random() *availableOptions.length)];
+		//pega a posição das option
+		const index2 = availableOptions.indexOf(optonIndex);
+		//remove option para não repetir
+		availableOptions.splice(index2,1);
+		console.log(optonIndex)
+		
+		const option = document.createElement("div");
+		option.innerHTML = currentQuestion.options[optonIndex];
+		option.id = optonIndex;
+		option.className = "option";
+		optionContainer.appendChild(option)
+		option.setAttribute("onclick","getResult(this)");
+	
+	}
+	questionCounter++;
+	console.log(questionCounter);
+	
+	
+	//console.log(availableOptions);
+	//console.log(currentQuestion.options)	
+	//console.log(index1);
+	console.log(questionIndex);
+	//console.log(availableQuestions);
+	
+	
+}
+
+
+function next(){
+	if (questionCounter === quiz.length){
+		ultimaP();
+		
+		console.log("estourou o vetor");
+		
+	}
+	else {
+		getNewQuestion();
+		answersIndicatorConteiner.innerHTML = '';   // limpa tela
+	}
+
+}
+
+var premio = 0;
+function stop(){
+	if (questionCounter == 6){
+		    premio -= 10
+		}
+		if ((questionCounter > 6)&&(questionCounter <=10)) {
+            premio -=10;
+        }
+		
+		if (questionCounter == 11){
+		    premio -= 100
+		}
+		
+		if ((questionCounter > 11)&&(questionCounter <=15)) {
+            premio -=100;
+        }
+		if (questionCounter ==16)
+			premio = 500;
+		
+		if (questionCounter <6)
+			premio --;
+		
+		if (premio < 0) 
+			premio = 0;
+	
+	answersIndicatorConteiner.innerHTML = " Seu prémio é de " + premio + " mil " ;
+   	closeBox.classList.add("hide"); // fecha a tela de perguntas.
+    
+	finalBox.classList.remove("hide");
+	finalBox.querySelector(".total-premio").innerHTML = premio + " mil";
+}
+
+
+
+// verifica se a resposta está correta
+
+function getResult(element){
+	const id = parseInt(element.id);
+	
+	if(id === currentQuestion.answer){
+		console.log(" Certa resposta! :)");
+		element.classList.add("correta");
+		
+		// roda na question correta
+		
+		if (questionCounter == 6){
+		    premio = 10
+		}
+		if ((questionCounter > 6)&&(questionCounter <=10)) {
+            premio +=10;
+        }
+		
+		if (questionCounter == 11){
+		    premio = 100
+		}
+		
+		if ((questionCounter > 11)&&(questionCounter <=15)) {
+            premio +=100;
+        }
+		if (questionCounter ==16){
+			premio *=2;
+		
+			finalBox.classList.remove("hide");
+	        finalBox.querySelector(".total-premio").innerHTML =" 1 MILHÃO DE REAIS! Parabens!!! ";
+			 document.querySelector(".next-question-btn").remove();// remove os button para encerrar o jogo
+            	
+
+		
+		
+		}
+		if (questionCounter <6)
+			premio ++;
+		answersIndicatorConteiner.innerHTML = " Certa resposta! seu prémio atual é de " + premio + " mil!" ;
+		
+		
+		
+	
+		
+	}
+	else{
+		console.log(" Resposta errada. :(");
+		element.classList.add("errada");
+		premio = premio /2; // divide o prémio se errar
+		answersIndicatorConteiner.innerHTML = " Você errou! Seu prémio é de " + premio + " mil !" ;
+    // ERRADA
+	
+		
+		
+		const optionLen = optionContainer.children.length;
+		for (let i=0;i<optionLen;i++){
+			if (parseInt(optionContainer.children[i].id)=== currentQuestion.answer){
+				optionContainer.children[i].classList.add("correta");
+				
+				
+			 document.querySelector(".next-question-btn").remove();// remove os button para encerrar o jogo
+			 //closeBox.classList.add("hide"); // fecha a tela de perguntas.
+			 finalBox.classList.remove("hide");
+	         finalBox.querySelector(".total-premio").innerHTML = premio + " mil";
+             
+			}
+		}
+
+
+	}
+	
+			
+	//const id = parseInt(element.id);
+	//console.log(typeof id);
+	//console.log(element.innerHTML);//mostra a resposta
+    //console.log(element.id);	//mostra o número (13:30)
+	
+	unclickableOptions();
+}
+
+
+//deixa as outras opções desclicaveis na questão  
+function unclickableOptions(){ 
+	const optionLen = optionContainer.children.length;
+   	for (let i=0; i < optionLen ; i++){
+		optionContainer.children[i].classList.add("already-answered");
+    
+	//console.log(optionContainer.children[i])
+	}
+}
+
+
+
+
+window.onload = function(){
+	setAvailableQuestions(); // setAvailableQuestions()
+	getNewQuestion();
+
+	
+	//ultimaP();
+	
+}
---
+++ js/question.js
@@ -0,0 +1,90 @@
+//listas de perguntas(array de objetos)
+var quiz = [
+
+{//1                   nível A - vale 1k
+	q:'Onde reina o personagem Pantera Negra?',
+	options:['Cidade de Deus','Paris','Wakanda','Bahia'],
+	answer:2
+},
+{
+	q:'Qual é o nome do cavalo do desenho O Show do Pica-Pau?',
+	options:['Rafael','Pé de pano','Cavalo de fogo','Bolsonaro'],
+	answer:1
+},
+{
+	q:'Quantos anos em média o cachorro vive?',
+	options:['30','3 - 7','10 - 13','16 - 20'],
+	answer:2
+},
+{
+	q:'O que é a África?',
+	options:['Um país','Uma Cidade','Um estado','Um continente'],
+	answer:3
+},
+{//5
+	q:'Em qual continente fica a Austrália?',
+	options:['América do Sul','Europa','África','Oceania'],
+	answer:3
+},
+{                      // nível B - vale 10k
+	q:'Qual é a forma da molécula de água?',
+	options:['H2O','CO2','CH4','TCC'],
+	answer:0
+},
+{
+	q:'Quem foi o primeiro presidente negro do Brasil?',
+	options:['Dilma','Nilo peçanha','Juscelino Kubitschek','Getúlio Vargas'],
+	answer:1
+},
+{
+	q:'De quem é a famosa frase “Penso, logo existo”?',
+	options:['Platão','Galileu Galilei','Descartes','Sócrates'],
+	answer:2
+},
+{
+	q:'De onde é a invenção do chuveiro elétrico?',
+	options:['França','Inglaterra','Brasil','Itália'],
+	answer:2
+},
+{
+	q:'Qual é o nome da mulher do líder quilombola, Zumbi dos Palmares?',
+	options:['Dandara','Damares','Beyoncé','Maria'],
+	answer:0
+},
+                            //NÍVEL C - vale 100K
+{
+	q:'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+	options:['Japão e Serra Leoa','Brasil e Congo','Estados Unidos e Angola','Austrália e Afeganistão'],
+	answer:0
+},
+{
+	q:'Quantas casas decimais tem o número pi?',
+	options:['Centenas','Milhares','Vinte','Infinitas'],
+	answer:3
+},
+{
+	q:'Qual a velocidade da luz??',
+	options:['300 000 000 metros por segundo (m/s)',' 299 792 458 metros por segundo (m/s)','199 792 458 metros por segundo (m/s)','30 000 000 metros por segundo (m/s)'],
+	answer:1
+},
+{
+    q:'Em qual local da Ásia o português é língua oficial?',
+	options:['Índia','Moçambique','Filipinas','Macau'],
+	answer:3
+},
+{
+    q:'Em qual data é comemorado o Dia da Consciência Negra?',
+	options:['26 de Outubro','12 de Fevereiro','20 de Novembro','04 de Junho'],
+	answer:2
+},
+
+
+ 
+{                        // Pergunta do Milhão
+	q:'Qual organização juvenil foi fundado por Baden-Powell?',
+    options:['A juventude socialista','O escotismo','O clube dos aventureiros','A Organização mundial da juventude'],
+    answer:1	
+} 
+
+]
+
